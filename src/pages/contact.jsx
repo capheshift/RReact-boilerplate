1e8bf3efe752d34f7814f67109b19a62b1da981e
Tam Pham <tampham47@live.com>
2015-08-22 16:51:40 +0700

    Custome source from react-flux-router-boilerplate

---
+++ src/pages/contact.jsx
@@ -0,0 +1,27 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react');
+var DefaultLayout = React.createFactory(require('../layouts/Default'));
+
+var ContactPage = React.createClass({
+  displayName: 'Contact page',
+
+  getDefaultProps: function() {
+    return {
+      layout: DefaultLayout
+    };
+  },
+
+  render: function() {
+    return (
+      <div>
+        <p>CONTACT PAGE</p>
+      </div>
+    );
+  }
+});
+
+module.exports = ContactPage;
